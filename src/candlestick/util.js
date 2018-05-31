621fb525128cc84b5870a4ec7ccafdb30b353246
t4sk <tsk.nakamura@gmail.com>
2018-05-31 23:10:03 +0900

    to y

--- src/candlestick/util.js
+++ src/candlestick/util.js
@@ -2,3 +2,15 @@
 export function floor(x: number): number {
   return x ^ 0
 }
+
+type ToYArgs = {
+  canvasHeight: number,
+  canvasY: number,
+  yMin: number,
+  yMax: number,
+}
+
+// convert canvas y to data point y
+export function toY({canvasHeight, canvasY, yMin, yMax}: ToYArgs): number {
+  return (yMax - yMin) / canvasHeight * canvasY + yMin
+}
--- src/candlestick/util.test.js
+++ src/candlestick/util.test.js
@@ -1,5 +1,31 @@
-import {floor} from './util'
+import {floor, toY} from './util'
 
 test("floor", () => {
   expect(floor(1.9)).toEqual(1)
+})
+
+test("convert canvas Y to data point Y", () => {
+  // y max
+  expect(floor(toY({
+    canvasHeight: 250,
+    canvasY: 250,
+    yMin: 10,
+    yMax: 110
+  }))).toEqual(110)
+
+  // y min
+  expect(floor(toY({
+    canvasHeight: 250,
+    canvasY: 0,
+    yMin: 10,
+    yMax: 110
+  }))).toEqual(10)
+
+  // between y min and max
+  expect(floor(toY({
+    canvasHeight: 250,
+    canvasY: 125,
+    yMin: 10,
+    yMax: 110
+  }))).toEqual(60)
 })
