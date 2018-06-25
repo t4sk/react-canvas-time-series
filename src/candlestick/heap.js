795916704d34e76388c139847197668622e80464
t4sk <tsk.nakamura@gmail.com>
2018-06-25 12:36:15 +0900

    heap

--- src/candlestick/heap.js
+++ src/candlestick/heap.js
@@ -1,7 +1,3 @@
-function fromArray(arr, sort = 1) {
-
-}
-
 function bubbleUp(arr, val, sort = 1) {
   arr.push(val)
 
@@ -29,24 +25,71 @@ function bubbleUp(arr, val, sort = 1) {
   }
 }
 
-function bubbleDown(arr, sort = 1) {
+function heapify(arr, i, sort = 1) {
+  while (true) {
+    let swap = i
+    let left = 2 * i
+    let right = 2 * i + 1
+
+    if (
+      left < arr.length && (
+        (sort == 1 && arr[left] > arr[swap]) ||
+        (sort == -1 && arr[left] < arr[swap])
+      )
+    ) {
+      swap = left
+    }
+    if (
+      right < arr.length && (
+        (sort == 1 && arr[right] > arr[swap]) ||
+        (sort == -1 && arr[right] < arr[swap])
+      )
+    ) {
+      swap = right
+    }
+
+    if (i == swap) {
+      return
+    }
+
+    const parent = arr[i]
+    arr[i] = arr[swap]
+    arr[swap] = parent
+
+    i = swap
+  }
 }
 
-class Heap {
+function bubbleDown(arr, sort = 1) {
+  arr[0] = arr.pop()
+  heapify(arr, 0, sort)
+}
+
+function buildHeap(arr = [], sort = 1) {
+  for (let i = (arr.length / 2 >> 0); i >= 0; i--) {
+    heapify(arr, i, sort)
+  }
+}
+
+export default class Heap {
   constructor(arr = [], sort = 1) {
     this.sort = sort
-    this.heap = fromArray(arr, sort)
+    this.heap = buildHeap(arr, sort)
   }
 
-  insert(val) {
-
-  }
-
-  getRoot() {
-
+  push(val) {
+    bubbleUp(this.heap, val, this.sort)
   }
 
   pop() {
+    const head = this.heap[0]
 
+    bubbleDown(this.heap, this.sort)
+
+    return head
+  }
+
+  peek() {
+    return this.heap[0]
   }
 }
