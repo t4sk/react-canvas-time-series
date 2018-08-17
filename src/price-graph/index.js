cf03a8861df8e3c625ece662ba9d9599180f09d3
t4sk <tsk.nakamura@gmail.com>
2018-08-17 22:15:17 +0900

    price graph: draw background

--- src/price-graph/index.js
+++ src/price-graph/index.js
@@ -1,15 +1,209 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import {round} from '../util'
+// import {drawData} from './data'
+import {drawBackground} from './background'
+// import {drawUI} from './ui'
 
 class PriceGraph extends Component {
+  componentDidMount() {
+    this.ctx = {
+      dataLayer: this.refs.dataLayer.getContext("2d"),
+      backgroundLayer: this.refs.backgroundLayer.getContext("2d"),
+    }
+
+    // TODO remove me
+    // TODO render at 10 ms
+    // setInterval(() => {
+    //   genFakeData()
+    //   if (DATA.length > 50) {
+    //     DATA.shift()
+    //   }
+    //   this.draw()
+    // }, 100)
+
+    // translate by half pixel to draw thin lines
+    this.ctx.backgroundLayer.translate(0.5, 0.5)
+    this.draw()
+  }
+
+  shouldComponentUpdate() {
+    // let canvas render
+    return false
+  }
+
+  getMetrics() {
+    const minTimestamp = DATA[0].timestamp
+    const maxTimestamp = DATA[DATA.length - 1].timestamp
+    const xInterval = Math.ceil((maxTimestamp - minTimestamp) / (DATA.length - 1))
+    const xMin = minTimestamp - round(xInterval / 2)
+    const xMax = maxTimestamp + round(xInterval / 2)
+    const minLow = Math.min(...DATA.map(d => d.low))
+    const maxHigh = Math.max(...DATA.map(d => d.high))
+    // yInterval >= ceil((yMax - yMin) / (num intervals - 2))
+    const yInterval = Math.ceil((maxHigh - minLow) / (this.props.background.numVerticalIntervals - 2))
+    const yMin = minLow - yInterval
+    const yMax = maxHigh + yInterval
+    const maxVolume = Math.max(...DATA.map(price => price.volume))
+
+    return {
+      maxVolume,
+      xMin,
+      xMax,
+      yMin,
+      yMax,
+      xInterval,
+      yInterval,
+    }
+  }
+
+  draw() {
+    this.ctx.dataLayer.clearRect(
+      0, 0,
+      this.ctx.backgroundLayer.canvas.width,
+      this.ctx.backgroundLayer.canvas.height
+    )
+
+    const metrics = this.getMetrics()
+
+    // background layer-
+    drawBackground(
+      this.ctx.backgroundLayer, {
+      ...this.props,
+      ...metrics,
+    })
+
+    // data layer
+    // drawData(this.ctx.dataLayer, {
+    //   ...this.props,
+    //   ...metrics,
+    // }, DATA)
+    //
+    // // ui layer
+    // drawUI(this.ctx.uiLayer, {
+    //   ...this.props,
+    //   ...metrics,
+    // }, this.mouse, DATA)
+  }
+
   render() {
     return (
-      <div>price graph</div>
+      <div style={style.container}>
+        <canvas
+          style={style.backgroundLayer}
+          ref="backgroundLayer"
+          width={this.props.width}
+          height={this.props.height}
+        >
+        </canvas>
+        <canvas
+          style={style.dataLayer}
+          ref="dataLayer"
+          width={this.props.width - this.props.background.yAxisPaddRight}
+          height={this.props.height - this.props.background.xAxisPaddBottom}
+        >
+        </canvas>
+      </div>
     )
   }
 }
 
+const style = {
+  container: {
+    position: "relative",
+  },
+  backgroundLayer: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    zIndex: 1
+  },
+  dataLayer: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    zIndex: 2,
+  },
+}
+
+PriceGraph.defaultProps = {
+  width: 500,
+  height: 300,
+  background: {
+    color: "#2f3d45",
+    xAxisPaddBottom: 50,
+    yAxisPaddRight: 50,
+    numVerticalIntervals: 6,
+    numHorizontalIntervals: 6,
+  },
+  candlestick: {
+    bull: {
+      color: "lightgreen",
+    },
+    bear: {
+      color: "red"
+    },
+  },
+  volumeBarChart: {
+    // TODO compute volume bar chart height from easier setting than pixel
+    height: 73,
+  },
+}
+
 PriceGraph.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  background :PropTypes.shape({
+    color: PropTypes.string.isRequired,
+    yAxisPaddRight: PropTypes.number.isRequired,
+    xAxisPaddBottom: PropTypes.number.isRequired,
+    numHorizontalIntervals: PropTypes.number.isRequired,
+    numVerticalIntervals: PropTypes.number.isRequired,
+  }).isRequired,
 }
 
 export default PriceGraph
+
+let DATA = []
+for (let i = 0; i < 50; i++) {
+  const high = randInt(60, 100)
+  const low = randInt(0, 30)
+  const open = randInt(low, high)
+  const close = randInt(low, high)
+  const volume = randInt(0, 100)
+
+  DATA[i] = {
+    high,
+    low,
+    open,
+    close,
+    timestamp: Date.now() - (50 * 100) + 100 * i,
+    volume,
+  }
+}
+
+// TODO remove me
+function genFakeData() {
+  const high = randInt(60, 100)
+  const low = randInt(0, 30)
+  const open = randInt(low, high)
+  const close = randInt(low, high)
+  const volume = randInt(0, 100)
+
+  DATA.push({
+    high,
+    low,
+    open,
+    close,
+    timestamp: Date.now(),
+    volume,
+  })
+}
+
+function rand(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+function randInt(min, max) {
+  return Math.floor(rand(min, max))
+}
