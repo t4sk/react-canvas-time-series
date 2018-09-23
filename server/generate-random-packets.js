c8595683be5ab58c802ddbc4bb1bf9411d0aec24
t4sk <tsk.nakamura@gmail.com>
2018-09-23 20:02:53 +0900

    function to generate packet

--- server/generate-random-packets.js
+++ server/generate-random-packets.js
@@ -2,62 +2,112 @@ const assert = require('assert')
 const flatbuffers = require('flatbuffers').flatbuffers
 const IbPacket = require('./packet_generated').IbPacket
 
-function main() {
-  const builder = new flatbuffers.Builder(0)
+function random(min, max) {
+  return Math.random() * (max - min) + min
+}
 
-  // Packet {
-  //   type: Tick
-  //   time: long
-  //   payload: Tick
-  // }
-  //
-  // Tick {
-  //   symb: string;
-  //   datetime: string;
-  //   open: double;
-  //   high: double;
-  //   low: double;
-  //   close: double;
-  //   volume: ulong;
-  //   wap: double;
-  //   count: uint;
-  // }
+function randomInt(min, max) {
+  return Math.floor(random(min, max))
+}
 
-  const symb = builder.createString("HUA")
-  const datetime = builder.createString("09:34:03-04")
-  const volume = builder.createLong(0, 1000)
+// Tick {
+//   symb: string;
+//   datetime: string;
+//   open: double;
+//   high: double;
+//   low: double;
+//   close: double;
+//   volume: ulong;
+//   wap: double;
+//   count: uint;
+// }
+
+function buildPayload(builder, args) {
+  let {
+    symb,
+    datetime,
+    open,
+    high,
+    low,
+    close,
+    volume,
+    wap,
+    count
+  } = args
+
+  symb = builder.createString(symb)
+  datetime = builder.createString(datetime)
+  volume = builder.createLong(0, volume)
 
   IbPacket.Tick.startTick(builder)
   IbPacket.Tick.addSymb(builder, symb)
   IbPacket.Tick.addDatetime(builder, datetime)
-  IbPacket.Tick.addOpen(builder, 100)
-  IbPacket.Tick.addHigh(builder, 200)
-  IbPacket.Tick.addLow(builder, 50)
-  IbPacket.Tick.addClose(builder, 150)
+  IbPacket.Tick.addOpen(builder, open)
+  IbPacket.Tick.addHigh(builder, high)
+  IbPacket.Tick.addLow(builder, low)
+  IbPacket.Tick.addClose(builder, close)
   IbPacket.Tick.addVolume(builder, volume)
-  IbPacket.Tick.addWap(builder, 100)
-  IbPacket.Tick.addCount(builder, 10)
-  const payload = IbPacket.Tick.endTick(builder)
+  IbPacket.Tick.addWap(builder, wap)
+  IbPacket.Tick.addCount(builder, count)
 
-  const now = builder.createLong(0, Date.now() / 1000)
+  return IbPacket.Tick.endTick(builder)
+}
+
+// Packet {
+//   type: Tick
+//   time: long
+//   payload: Tick
+// }
+//
+
+function buildPacket(args = {}) {
+  let {
+    time,
+    payload,
+  } = args
+
+  const builder = new flatbuffers.Builder(0)
+
+  payload = buildPayload(builder, payload)
+  time = builder.createLong(0, time)
 
   IbPacket.Packet.startPacket(builder)
   IbPacket.Packet.addType(builder, IbPacket.Type.Tick)
-  IbPacket.Packet.addTime(builder, now)
+  IbPacket.Packet.addTime(builder, time)
   IbPacket.Packet.addPayloadType(builder, IbPacket.Data.Tick);
   IbPacket.Packet.addPayload(builder, payload)
   const packet = IbPacket.Packet.endPacket(builder)
 
   builder.finish(packet)
 
-  // Read
-  const buf = builder.dataBuffer()
+  return builder.dataBuffer()
+}
 
+function main() {
+  const now = Date.now() / 1000
+
+  const buf = buildPacket({
+    time: now,
+    payload: {
+      symb: "HUA",
+      datetime: "09:34:03-04",
+      volume: 1000,
+      open: 100,
+      high: 200,
+      low: 50,
+      close: 150,
+      volume: 1000,
+      wap: 100,
+      count: 10,
+    }
+  })
+
+  // Read
   const pkt = IbPacket.Packet.getRootAsPacket(buf)
 
   assert.equal(pkt.type(), IbPacket.Type.Tick)
-  assert.equal(pkt.time().low, now.low)
-  assert.equal(pkt.time().high, now.high)
+  assert.equal(pkt.time().low, 0)
+  assert.equal(pkt.time().high, Math.floor(now))
   assert.equal(pkt.payloadType(), IbPacket.Data.Tick)
   const tick = new IbPacket.Tick()
   assert.equal(pkt.payload(tick).symb(), "HUA")
