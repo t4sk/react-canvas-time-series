63a42947c24ffa1f2cbf22700649c07dc44f4947
t4sk <tsk.nakamura@gmail.com>
2018-09-25 07:28:46 +0900

    flow util

--- src/common/util.js
+++ src/common/util.js
@@ -1,16 +1,33 @@
-export function floor(x) {
+//@flow
+
+export function floor(x: number): number {
   return x ^ 0
 }
 
-export function round(x) {
+export function round(x: number): number {
   return (x + 0.5) | 0
 }
 
-export function linear({dx, dy, x, y0}) {
+type LinearArgs = {
+  dx: number,
+  dy: number,
+  x: number,
+  y0: number,
+}
+
+export function linear({dx, dy, x, y0}: LinearArgs): number {
   return dy / dx * x + y0
 }
 
-export function linearTransformer({dx, dy, y0}) {
+type LinearTransformerArgs = {
+  dx: number,
+  dy: number,
+  y0: number,
+}
+
+export function linearTransformer(
+  {dx, dy, y0}: LinearTransformerArgs
+): (x: number) => number {
   const df = dy / dx
   return x => df * x + y0
 }
--- src/common/util.test.js
+++ src/common/util.test.js
@@ -1,3 +1,4 @@
+// @flow
 import {floor, round, linear, linearTransformer} from './util'
 
 test("floor", () => {
