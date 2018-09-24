a89f437ba1597d3c49a6b3f8596418cc6d8508f2
t4sk <tsk.nakamura@gmail.com>
2018-09-24 17:35:45 +0900

    generate random packet

--- server/generate-random-packet.js
+++ server/generate-random-packet.js
@@ -2,14 +2,6 @@ const assert = require('assert')
 const flatbuffers = require('flatbuffers').flatbuffers
 const IbPacket = require('./packet_generated').IbPacket
 
-function random(min, max) {
-  return Math.random() * (max - min) + min
-}
-
-function randomInt(min, max) {
-  return Math.floor(random(min, max))
-}
-
 // Tick {
 //   symb: string;
 //   datetime: string;
@@ -83,45 +75,56 @@ function buildPacket(args = {}) {
   return builder.dataBuffer()
 }
 
-function main() {
-  const now = Date.now() / 1000
-
-  const buf = buildPacket({
-    time: now,
-    payload: {
-      symb: "HUA",
-      datetime: "09:34:03-04",
-      volume: 1000,
-      open: 100,
-      high: 200,
-      low: 50,
-      close: 150,
-      volume: 1000,
-      wap: 100,
-      count: 10,
-    }
-  })
-
-  // Read
-  const pkt = IbPacket.Packet.getRootAsPacket(buf)
-
-  assert.equal(pkt.type(), IbPacket.Type.Tick)
-  assert.equal(pkt.time().low, 0)
-  assert.equal(pkt.time().high, Math.floor(now))
-  assert.equal(pkt.payloadType(), IbPacket.Data.Tick)
-  const tick = new IbPacket.Tick()
-  assert.equal(pkt.payload(tick).symb(), "HUA")
-  assert.equal(pkt.payload(tick).datetime(), "09:34:03-04")
-  assert.equal(pkt.payload(tick).open(), 100)
-  assert.equal(pkt.payload(tick).high(), 200)
-  assert.equal(pkt.payload(tick).low(), 50)
-  assert.equal(pkt.payload(tick).close(), 150)
-  assert.equal(pkt.payload(tick).volume().low, 0)
-  assert.equal(pkt.payload(tick).volume().high, 1000)
-  assert.equal(pkt.payload(tick).wap(), 100)
-  assert.equal(pkt.payload(tick).count(), 10)
-
-  console.log('The FlatBuffer was successfully created and verified!')
+function random(min, max) {
+  return Math.random() * (max - min) + min
 }
 
-main()
+function randomInt(min, max) {
+  return Math.floor(random(min, max))
+}
+
+const SYMBOLS = [
+  "AAA",
+  "BBB",
+  "CCC",
+  "DDD",
+  "EEE",
+  "FFF",
+  "GGG",
+  "HHH",
+]
+
+function randomSymbol() {
+  return SYMBOLS[randomInt(0, SYMBOLS.length - 1)]
+}
+
+function generateRandomPacket() {
+  const now = new Date()
+
+  const low = random(0, 1000)
+  const high = random(low, low + 1000)
+  const open = random(low, high)
+  const close = random(low, high)
+
+  const args = {
+    time: Math.round(now.valueOf() / 1000),
+    payload: {
+      symb: randomSymbol(),
+      datetime: now.toString().substring(16, 24),
+      open,
+      high,
+      low,
+      close,
+      volume: randomInt(100, 1000),
+      wap: randomInt(0, 100),
+      count: randomInt(0, 100),
+    }
+  }
+
+  return buildPacket(args)
+}
+
+module.exports = {
+  generateRandomPacket,
+  buildPacket,
+}
