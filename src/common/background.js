c5cdcd3b705fed24bb6d75a288b3d91e1c009957
t4sk <tsk.nakamura@gmail.com>
2018-09-26 07:33:48 +0900

    draw x lines

--- src/common/background.js
+++ src/common/background.js
@@ -1,4 +1,69 @@
+// @flow
 import { round, linear } from './math'
+
+function getWidth(props) {
+  return props.width - props.y.axis.width
+}
+
+function getHeight(props) {
+  return props.height - props.x.axis.height - (props.margin.top + props.margin.bottom)
+}
+
+function getXLineCanvasYStart (props) {
+  switch (props.x.axis.at) {
+    case 'top':
+      return props.x.axis.height + props.margin.top
+    case 'bottom':
+      return props.margin.top
+    default:
+      throw new Error(`invalid x.axis.at ${props.x.axis.at}`)
+  }
+}
+
+function getXLineCanvasXStart (props) {
+  switch (props.y.axis.at) {
+    case 'left':
+      return props.y.axis.width
+    case 'right':
+      return 0
+    default:
+      throw new Error(`invalid y.axis.at ${props.y.axis.at}`)
+  }
+}
+
+function drawXLines(ctx, props) {
+  const { xMin, xMax } = props
+
+  const width = getWidth(props)
+  const height = getHeight(props)
+  const interval = width / props.x.intervals
+  const toX = linear({
+    dy: xMax - xMin,
+    dx: width,
+    y0: xMin
+  })
+
+  const xLineCanvasYStart = getXLineCanvasYStart(props)
+  const xLineCanvasXStart = getXLineCanvasXStart(props)
+
+  for (let i = 0; i <= props.x.intervals; i++) {
+    const canvasX = round(i * interval) + xLineCanvasXStart
+
+    // draw line
+    ctx.moveTo(canvasX, xLineCanvasYStart)
+    ctx.lineTo(canvasX, xLineCanvasYStart + height)
+    ctx.stroke()
+
+    // draw text
+    // const x = round(toX(canvasX))
+    // const date = new Date(x)
+    // ctx.fillText(
+    //   `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    //   canvasX,
+    //   height + 10
+    // )
+  }
+}
 
 function getYAxisTextAlign (props) {
   switch (props.y.axis.at) {
@@ -19,6 +84,17 @@ function getYAxisCanvasX (props) {
       return 0
     default:
       throw new Error(`invalid y.axis.at ${props.y.axis.at}`)
+  }
+}
+
+function getYLineCanvasYStart (props) {
+  switch (props.x.axis.at) {
+    case 'top':
+      return props.x.axis.height
+    case 'bottom':
+      return 0
+    default:
+      throw new Error(`invalid x.axis.at ${props.x.axis.at}`)
   }
 }
 
@@ -48,8 +124,8 @@ function drawYLines (ctx, props) {
   ctx.textBaseline = 'middle'
   ctx.textAlign = getYAxisTextAlign(props)
 
-  const width = props.width - props.y.axis.width
-  const height = props.height - props.x.axis.height - (props.margin.top + props.margin.bottom)
+  const width = getWidth(props)
+  const height = getHeight(props)
   const interval = height / props.y.intervals
   const toY = linear({
     dy: yMax - yMin,
@@ -59,9 +135,10 @@ function drawYLines (ctx, props) {
 
   const yAxisCanvasX = getYAxisCanvasX(props)
   const labelCanvasX = getLabelCanvasX(props)
+  const yLineCanvasYStart = getYLineCanvasYStart(props)
 
   for (let i = 0; i <= props.y.intervals; i++) {
-    const canvasY = round(i * interval) + props.margin.top
+    const canvasY = round(i * interval) + props.margin.top + yLineCanvasYStart
 
     // draw line
     ctx.moveTo(yAxisCanvasX, canvasY)
@@ -85,5 +162,6 @@ export function draw (ctx, props) {
   // style lines
   ctx.lineWidth = 1
 
+  drawXLines(ctx, props)
   drawYLines(ctx, props)
 }
