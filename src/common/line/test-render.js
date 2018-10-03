28ded5b7c745a9bb7ae272ec4281ee436ddc111c
t4sk <tsk.nakamura@gmail.com>
2018-10-04 07:20:43 +0900

    increase random data

--- src/common/line/test-render.js
+++ src/common/line/test-render.js
@@ -15,7 +15,7 @@ const DATA = [{
 
 let RANDOM_DATA = []
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 100; i++) {
   RANDOM_DATA.push({
     x: rand(X_MIN, X_MAX),
     y: rand(Y_MIN, Y_MAX)
