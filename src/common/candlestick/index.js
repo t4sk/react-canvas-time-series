f19eb2c5b7e173ab299c81357aa351295cc680ae
t4sk <tsk.nakamura@gmail.com>
2018-10-05 21:10:38 +0900

    candlestick

---
+++ src/common/candlestick/index.js
@@ -0,0 +1,84 @@
+// @flow
+import { round, linear } from '../math'
+import type { Props } from './types'
+
+export function draw (ctx: any, props: Props) {
+  const {
+    yMin,
+    yMax,
+    data,
+    graph
+  } = props
+
+  const toCanvasY = linear({
+    dy: -graph.height,
+    dx: yMax - yMin,
+    y0: graph.y + graph.height * yMax / (yMax - yMin)
+  })
+
+  const xInterval = graph.width / data.length
+  const barWidth = Math.max(xInterval, 1)
+
+  for (let i = 0; i < data.length; i++) {
+    const {
+      high,
+      low,
+      open,
+      close,
+    } = data[i]
+
+    const x = graph.x + i * xInterval
+    const yTop = toCanvasY(Math.max(open, close))
+    const yBottom = toCanvasY(Math.min(open, close))
+
+    const barHeight = Math.max(yBottom - yTop, 1)
+
+    // body
+    ctx.fillStyle = props.candlestick.body.getBackgroundColor(data[i])
+
+    ctx.fillRect(
+      round(x),
+      round(yTop),
+      round(barWidth),
+      round(barHeight)
+    )
+
+    ctx.strokeStyle = props.candlestick.body.line.getColor(data[i])
+    ctx.lineWidth = props.candlestick.body.line.width
+
+    ctx.beginPath()
+    ctx.rect(
+      round(x),
+      round(yTop),
+      round(barWidth),
+      round(barHeight)
+    )
+    ctx.stroke()
+
+    // wick
+    ctx.strokeStyle = props.candlestick.wick.getColor(data[i])
+    ctx.lineWidth = props.candlestick.wick.width
+
+    // top wick
+    ctx.beginPath()
+    ctx.moveTo(
+      round(x + barWidth / 2),
+      round(yTop)
+    )
+    ctx.lineTo(
+      round(x + barWidth / 2),
+      round(toCanvasY(high))
+    )
+
+    // bottom wick
+    ctx.moveTo(
+      round(x + barWidth / 2),
+      round(yTop + barHeight)
+    )
+    ctx.lineTo(
+      round(x + barWidth / 2),
+      round(toCanvasY(low))
+    )
+    ctx.stroke()
+  }
+}
---
+++ src/common/candlestick/test-render.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react'
+import { rand } from '../test-util'
+import TestCanvas from '../test-canvas'
+import * as candlestick from './index'
+
+const X_MIN = 1900
+const X_MAX = 2018
+const Y_MIN = 0
+const Y_MAX = 100
+
+function generateRandomData(length) {
+  let data = []
+
+  for (let i = 0; i < length; i++) {
+    const high = rand(Y_MIN, Y_MAX)
+    const low = rand(Y_MIN, high)
+    const open = rand(low, high)
+    const close = rand(low, high)
+
+    data.push({
+      high,
+      low,
+      open,
+      close
+    })
+  }
+
+  return data
+}
+
+const FIXED_DATA = [{
+  high: Y_MAX - 10,
+  low: Y_MIN,
+  open: Y_MIN + 10,
+  close: Y_MAX - 20,
+}, {
+  high: Y_MAX,
+  low: Y_MIN + 10,
+  open: Y_MAX - 10,
+  close: Y_MIN + 20,
+}]
+
+const RANDOM_DATA_SMALL = generateRandomData(10)
+const RANDOM_DATA_MEDIUM = generateRandomData(100)
+const RANDOM_DATA_LARGE = generateRandomData(1000)
+
+class TestRender extends Component {
+  render () {
+    return (
+      <div>
+        <h3>Candlestick (Fixed data)</h3>
+        <TestCanvas
+          {...this.props}
+          draw={candlestick.draw}
+          data={FIXED_DATA}
+        />
+
+        <h3>{`Candlestick (Random ${RANDOM_DATA_SMALL.length} data)`}</h3>
+        <TestCanvas
+          {...this.props}
+          draw={candlestick.draw}
+          data={RANDOM_DATA_SMALL}
+        />
+
+        <h3>{`Candlestick (Random ${RANDOM_DATA_MEDIUM.length} data)`}</h3>
+        <TestCanvas
+          {...this.props}
+          draw={candlestick.draw}
+          data={RANDOM_DATA_MEDIUM}
+        />
+
+        <h3>{`Candlestick (Random ${RANDOM_DATA_LARGE.length} data)`}</h3>
+        <TestCanvas
+          {...this.props}
+          draw={candlestick.draw}
+          data={RANDOM_DATA_LARGE}
+        />
+      </div>
+    )
+  }
+}
+
+TestRender.defaultProps = {
+  draw: (ctx, props) => {},
+  canvas: {
+    width: 500,
+    height: 300
+  },
+  graph: {
+    // y label left, x label bottom
+    x: 60, // margin.left + x.axis.width
+    y: 20, // margin.
+    width: 420, // canvas.width - (margin.left + margin.right + x.axis.width)
+    height: 220 // canvas.height - (margin.top + margin.bottom + y.axis.height)
+  },
+  candlestick: {
+    wick: {
+      width: 2,
+      getColor: (d) => d.open <= d.close ? 'blue' : 'orange'
+    },
+    body: {
+      line: {
+        width: 1,
+        getColor: (d) => d.open <= d.close ? 'green' : 'red',
+      },
+      getBackgroundColor: (d) => d.open <= d.close ? 'lightgreen' : 'pink'
+    }
+  },
+  data: [],
+  yMin: Y_MIN,
+  yMax: Y_MAX
+}
+
+export default TestRender
