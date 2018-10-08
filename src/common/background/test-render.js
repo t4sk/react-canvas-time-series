3fc0d1eef695951709515fff2cc6f07c02023d80
t4sk <tsk.nakamura@gmail.com>
2018-10-08 21:47:25 +0900

    use TestCanvas in Background render

--- src/common/background/test-render.js
+++ src/common/background/test-render.js
@@ -1,112 +1,65 @@
 import React, { Component } from 'react'
 import * as background from './index'
 import { merge } from '../test-util'
+import TestCanvas from '../test-canvas'
 
-class BackgroundTestRender extends Component {
-  componentDidMount () {
-    this.ctx = {
-      xAxisTop: this.refs.xAxisTop.getContext('2d', { alpha: false }),
-      xAxisBottom: this.refs.xAxisBottom.getContext('2d', { alpha: false }),
-      yAxisRight: this.refs.yAxisRight.getContext('2d', { alpha: false }),
-      yAxisLeft: this.refs.yAxisLeft.getContext('2d', { alpha: false })
-    }
-
-    // translate by half pixel to draw thin lines
-    this.ctx.xAxisTop.translate(0.5, 0.5)
-    this.ctx.xAxisBottom.translate(0.5, 0.5)
-    this.ctx.yAxisLeft.translate(0.5, 0.5)
-    this.ctx.yAxisRight.translate(0.5, 0.5)
-    this.draw()
-  }
-
-  shouldComponentUpdate () {
-    return false
-  }
-
-  draw () {
-    background.draw(
-      this.ctx.xAxisBottom, {
-        ...merge(this.props, {
-          x: {
-            axis: {
-              at: 'bottom'
-            }
-          }
-        })
-      }
-    )
-    background.draw(
-      this.ctx.xAxisTop, {
-        ...merge(this.props, {
-          x: {
-            axis: {
-              at: 'top'
-            }
-          }
-        })
-      }
-    )
-    background.draw(
-      this.ctx.yAxisLeft, {
-        ...merge(this.props, {
-          y: {
-            axis: {
-              at: 'left'
-            }
-          }
-        })
-      }
-    )
-
-    background.draw(
-      this.ctx.yAxisRight, {
-        ...merge(this.props, {
-          y: {
-            axis: {
-              at: 'right'
-            }
-          }
-        })
-      }
-    )
-  }
-
+class TestRender extends Component {
   render () {
     return (
       <div>
         <h3>X Axis Bottom</h3>
-        <canvas
-          ref="xAxisBottom"
-          width={this.props.width}
-          height={this.props.height}
+        <TestCanvas
+          {...merge(this.props, {
+            x: {
+              axis: {
+                at: 'bottom'
+              }
+            }
+          })}
+          draw={background.draw}
         />
 
         <h3>X Axis Top</h3>
-        <canvas
-          ref="xAxisTop"
-          width={this.props.width}
-          height={this.props.height}
+        <TestCanvas
+          {...merge(this.props, {
+            x: {
+              axis: {
+                at: 'top'
+              }
+            }
+          })}
+          draw={background.draw}
         />
 
         <h3>Y Axis Left</h3>
-        <canvas
-          ref="yAxisLeft"
-          width={this.props.width}
-          height={this.props.height}
+        <TestCanvas
+          {...merge(this.props, {
+            y: {
+              axis: {
+                at: 'left'
+              }
+            }
+          })}
+          draw={background.draw}
         />
 
         <h3>Y Axis Right</h3>
-        <canvas
-          ref="yAxisRight"
-          width={this.props.width}
-          height={this.props.height}
+        <TestCanvas
+          {...merge(this.props, {
+            y: {
+              axis: {
+                at: 'right'
+              }
+            }
+          })}
+          draw={background.draw}
         />
       </div>
     )
   }
 }
 
-BackgroundTestRender.defaultProps = {
+TestRender.defaultProps = {
   width: 500,
   height: 300,
   margin: {
@@ -152,4 +105,4 @@ BackgroundTestRender.defaultProps = {
   xMax: 2010
 }
 
-export default BackgroundTestRender
+export default TestRender
