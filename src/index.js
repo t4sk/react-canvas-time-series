c8337a92d5dedbf1ad4ed377aa45ee7677d5d1d9
t4sk <tsk.nakamura@gmail.com>
2018-11-12 23:04:43 +0900

    graph canvas component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,158 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export class GraphCanvas extends Component {
   static propTypes = {
-    text: PropTypes.string
+    onMouseMove: PropTypes.func,
+    onMouseDown: PropTypes.func,
+    onMouseUp: PropTypes.func,
+    onMouseOut: PropTypes.func,
+    onWheel: PropTypes.func,
+    drawBackground: PropTypes.func.isRequired,
+    drawData: PropTypes.func.isRequired,
+    drawUI: PropTypes.func,
+    canvas: PropTypes.shape({
+      width: PropTypes.number,
+      height: PropTypes.number,
+    })
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  static defaultPropss = {
+    onMouseMove: (e, mouse) => {},
+    onMouseDown: (e, mouse) => {},
+    onMouseUp: (e, mouse) => {},
+    onMouseOut: (e, mouse) => {},
+    onWheel: (e, mouse) => {},
+    drawData: (ctx) => {},
+    drawBackground: (ctx) => {},
+    canvas: {
+      width: 500,
+      height: 300
+    },
+  }
 
+  constructor (props) {
+    super(props)
+
+    this.background = React.createRef()
+    this.data = React.createRef()
+    this.ui = React.createRef()
+
+    this.mouse = {
+      x: undefined,
+      y: undefined,
+    }
+
+    // ref to animation frame
+    this.animation = undefined
+  }
+
+  onMouseMove = (e) => {
+    const rect = this.ctx.ui.canvas.getBoundingClientRect()
+
+    this.mouse.x = e.clientX - rect.left
+    this.mouse.y = e.clientY - rect.top
+
+    this.props.onMouseMove(e, this.mouse)
+  }
+
+  onMouseDown = (e) => {
+    this.props.onMouseDown(e, this.mouse)
+  }
+
+  onMouseUp = (e) => {
+    this.props.onMouseUp(e, this.mouse)
+  }
+
+  onMouseOut = (e) => {
+    this.mouse.x = undefined
+    this.mouse.y = undefined
+
+    this.props.onMouseOut(e, this.mouse)
+  }
+
+  onWheel = (e) => {
+    this.props.onWheel(e, this.mouse)
+  }
+
+  componentDidMount () {
+    this.ctx = {
+      ui: this.ui.current.getContext('2d'),
+      data: this.data.current.getContext('2d'),
+      background: this.background.current.getContext('2d', { alpha: false })
+    }
+
+    if (this.props.drawUI) {
+      this.ctx.ui.canvas.addEventListener('mousemove', this.onMouseMove)
+      this.ctx.ui.canvas.addEventListener('mousedown', this.onMouseDown)
+      this.ctx.ui.canvas.addEventListener('mouseup', this.onMouseUp)
+      this.ctx.ui.canvas.addEventListener('mouseout', this.onMouseOut)
+      this.ctx.ui.canvas.addEventListener('wheel', this.onWheel)
+
+      this.animate()
+    } else {
+      this.draw()
+    }
+  }
+
+  componentWillUnmount() {
+    // TODO test unmount event listeners
+    this.ctx.ui.canvas.removeEventListener('mousemove', this.onMouseMove)
+    this.ctx.ui.canvas.removeEventListener('mousemove', this.onMouseMove)
+    this.ctx.ui.canvas.removeEventListener('mousemove', this.onMouseMove)
+    this.ctx.ui.canvas.removeEventListener('mousemove', this.onMouseMove)
+    this.ctx.ui.canvas.removeEventListener('mousemove', this.onMouseMove)
+    this.ctx.ui.canvas.removeEventListener('wheel', this.onWheel)
+
+    window.cancelAnimationFrame(this.animation)
+  }
+
+  shouldComponentUpdate (nextProps) {
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      nextProps.canvas.width !== this.props.canvas.width ||
+      nextProps.canvas.height !== this.props.canvas.height
+    )
+  }
+
+  draw = () => {
+    this.props.drawBackground(this.ctx.background)
+    this.props.drawData(this.ctx.data)
+  }
+
+  animate = () => {
+    this.animation = window.requestAnimationFrame(this.animate)
+
+    this.props.drawBackground(this.ctx.background)
+    this.props.drawData(this.ctx.data)
+
+    this.props.drawUI(this.ctx.ui, this.mouse)
+  }
+
+  render () {
+    return (
+      <div style={{
+        ...styles.container,
+        width: this.props.canvas.width,
+        height: this.props.canvas.height
+      }}>
+        <canvas
+          ref={this.background}
+          style={styles.background}
+          width={this.props.canvas.width}
+          height={this.props.canvas.height}
+        />
+        <canvas
+          ref={this.data}
+          style={styles.data}
+          width={this.props.canvas.width}
+          height={this.props.canvas.height}
+        />
+        <canvas
+          ref={this.ui}
+          style={styles.ui}
+          width={this.props.canvas.width}
+          height={this.props.canvas.height}
+        />
       </div>
     )
   }
