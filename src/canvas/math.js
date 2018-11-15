880e0d73ceb05a78f1e3634071f91a08fc1d1f6f
t4sk <tsk.nakamura@gmail.com>
2018-11-15 17:57:43 +0900

    math: findIndexOfNearestData

--- src/canvas/math.js
+++ src/canvas/math.js
@@ -12,10 +12,51 @@ export function nearestStepBelow (x: number, step: number): number {
   return Math.floor(x / step) * step
 }
 
+// TODO test
+export function findIndexOfNearestData (
+  x: number,
+  data: Array<{number}>
+): {x: number} {
+  let low = 0;
+  let high = data.length - 1
+
+  if (data.length <= 1) {
+    return high
+  }
+
+  if (data[low] > data[high]) {
+    throw "Data must be sorted in ascending order"
+  }
+
+  // binary search
+  while (low < high) {
+    let mid = (low + high) / 2 >> 0
+
+    if (data[mid] > x) {
+      high = mid
+    } else {
+      low = mid + 1
+    }
+  }
+
+  if (data[low - 1]) {
+    const midX = (data[low] + data[low - 1]) / 2
+
+    if (x < midX) {
+      return low - 1
+    } else {
+      return low
+    }
+  }
+
+  return low
+}
+
 export function getNearestDataAtX (
   x: number,
   data: Array<{x: number}>
 ): {x: number} {
+  console.log("deprecated: getNearestDataAtX, use findIndexOfNearestData")
   let low = 0;
   let high = data.length - 1
 
