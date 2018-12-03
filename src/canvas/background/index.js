ed1b5f842e1cd5809c2f6999f671bb5fbf848ddb
t4sk <tsk.nakamura@gmail.com>
2018-12-03 23:02:09 +0900

    todo

--- src/canvas/background/index.js
+++ src/canvas/background/index.js
@@ -3,6 +3,7 @@ import type { Props } from './types'
 import { drawXLines } from './x-lines'
 import { drawYLines } from './y-lines'
 
+// TODO? render x, y labels on both side
 export function draw (ctx: any, props) {
   ctx.fillStyle = props.backgroundColor
   ctx.fillRect(
