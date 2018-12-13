55f025d702301d4f64dd8658408fc062ed70344c
t4sk <tsk.nakamura@gmail.com>
2018-12-13 16:40:16 +0900

    remove canvas.fill

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -5,18 +5,7 @@ import * as line from './line'
 import * as ui from './ui'
 import * as math from './math'
 
-function fill (ctx: any, props) {
-  ctx.fillStyle = props.backgroundColor
-  ctx.fillRect(
-    0,
-    0,
-    props.width,
-    props.height,
-  )
-}
-
 export default {
-  fill,
   background,
   bar,
   candlestick,
