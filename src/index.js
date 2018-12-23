3e3ae75b979522bb7ac01d3eb78aa7a079308e8e
t4sk <tsk.nakamura@gmail.com>
2018-12-23 12:04:10 +0900

    remove todo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ export { default as GraphCanvas } from './graph-canvas'
 
 // TODO flow
 
-// TODO how to pass common props (canvas, graph, min, max,...) to each draw func.
-// TODO default props for background, bar, line, candlestick, ui
-// TODO pass (ctx, props) to this.props.drawBackground ?
-// TODO pass (ctx, props) to this.props.drawData ?
-// TODO pass (ctx, mouse, props) to this.props.drawUI ?
-// TODO? components for drawing
-
-// TODO? canvas border color
 // TODO fix flickering canvas border
 
 // TODO drag example with data
@@ -21,7 +13,6 @@ export { default as GraphCanvas } from './graph-canvas'
 // TODO reusable component for drag
 
 // TODO optimizations mentioned in mozilla website
-// TODO performance of spread operators when drawing
 // TODO highlight column
 // TODO draw tick
 
@@ -31,5 +22,4 @@ export { default as GraphCanvas } from './graph-canvas'
 // TODO compute barWidth from xStep
 
 // TODO ? set graph canvas size in render()
-// TODO remove bar.getBorderColor
 // TODO render ui labels near canvas borders
