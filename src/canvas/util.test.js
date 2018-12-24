f2615aa457756988f39ecb38254fd06c499a39d2
t4sk <tsk.nakamura@gmail.com>
2018-12-24 17:54:31 +0900

    fix util funcs

--- src/canvas/util.test.js
+++ src/canvas/util.test.js
@@ -1,89 +1,99 @@
 // @flow
-import { merge } from '../test-util'
-
 import {
-  getGraphWidth,
-  getGraphHeight,
-  getGraphLeft,
-  getGraphTop,
+  getGraphDimensions
 } from './util'
 
 const props = {
-  canvas: {
-    width: 500,
-    height: 300,
+  width: 500,
+  height: 300,
+  padding: {
+    top: 10,
+    bottom: 20,
+    left: 10,
+    right: 20
   },
   background: {
-    width: 460,
-    height: 270,
     xAxisAt: 'bottom',
     xAxisHeight: 50,
     yAxisAt: 'left',
     yAxisWidth: 50,
-  }
+  },
 }
 
-// TODO fix test
-test("get width", () => {
-  expect(getGraphWidth(props)).toEqual(
-    props.background.width - props.background.yAxisWidth
-  )
-})
-
-test("get height", () => {
-  expect(getGraphHeight(props)).toEqual(
-    props.background.height - props.background.xAxisHeight
-  )
-})
-
-describe("get graph y", () => {
-  test("x axis at top", () => {
-    expect(getGraphTop(merge(props, {
-      background: {
-        xAxisAt: 'top'
-      }
-    }))).toEqual(props.padding.top + props.background.xAxisHeight)
+describe("get graph dimensions", () => {
+  test("width", () => {
+    expect(getGraphDimensions(props).width).toEqual(
+      props.width - props.background.yAxisWidth - (props.padding.left + props.padding.right)
+    )
   })
 
-  test("x axis at bottom", () => {
-    expect(getGraphTop(merge(props, {
-      background: {
-        xAxisAt: 'bottom'
-      }
-    }))).toEqual(props.padding.top)
+  test("height", () => {
+    expect(getGraphDimensions(props).height).toEqual(
+      props.height - props.background.xAxisHeight - (props.padding.top + props.padding.bottom)
+    )
   })
 
-  test("invalid x axis", () => {
-    expect(() => getGraphTop(merge(props, {
-      background: {
-        xAxisAt: 'invalid'
-      }
-    }))).toThrow()
-  })
-})
+  describe("top", () => {
+    test("x axis at top", () => {
+      expect(getGraphDimensions({
+        ...props,
+        background: {
+          ...props.background,
+          xAxisAt: 'top'
+        }
+      }).top).toEqual(props.padding.top + props.background.xAxisHeight)
+    })
 
-describe("get graph x", () => {
-  test("y axis at left", () => {
-    expect(getGraphLeft(merge(props, {
-      background: {
-        yAxisAt: 'left'
-      }
-    }))).toEqual(props.padding.left + props.background.yAxisWidth)
+    test("x axis at bottom", () => {
+      expect(getGraphDimensions({
+        ...props,
+        background: {
+          ...props.background,
+          xAxisAt: 'bottom'
+        }
+      }).top).toEqual(props.padding.top)
+    })
+
+    test("invalid x axis", () => {
+      expect(() => getGraphDimensions({
+        ...props,
+        background: {
+          ...props.background,
+          xAxisAt: 'invalid'
+        }
+      })).toThrow()
+    })
   })
 
-  test("y axis at right", () => {
-    expect(getGraphLeft(merge(props, {
-      background: {
-        yAxisAt: 'right'
-      }
-    }))).toEqual(props.padding.left)
-  })
+  describe("left", () => {
+    test("y axis at left", () => {
+      expect(getGraphDimensions({
+        ...props,
+        background: {
+          ...props.background,
+          yAxisAt: 'left'
+        }
+      }).left).toEqual(props.padding.left + props.background.yAxisWidth)
+    })
 
-  test("invalid y axis", () => {
-    expect(() => getGraphLeft(merge(props, {
-      background: {
-        yAxisAt: 'invalid'
-      }
-    }))).toThrow()
+    test("y axis at right", () => {
+      expect(getGraphDimensions({
+        ...props,
+        background: {
+          ...props.background,
+          yAxisAt: 'right'
+        }
+      }).left).toEqual(props.padding.left)
+    })
+
+    test("invalid y axis", () => {
+      expect(() => getGraphDimensions({
+        ...props,
+        background: {
+          ...props.background,
+          yAxisAt: 'invalid'
+        }
+      })).toThrow()
+    })
   })
 })
