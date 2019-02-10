78b26d5f7b74714978d20131f846e7fd10ef6267
t4sk <tsk.nakamura@gmail.com>
2019-02-10 19:10:51 +0900

    delete files

--- example/src/App.js
+++ example/src/App.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react'
 import TestRenderHistory from './test-render/history'
 import TestRenderAxes from './test-render/axes'
-import BackgroundTestRender from './test-render/background'
 import TestRenderLine from './test-render/line'
-import PointTestRender from './test-render/point'
-import BarTestRender from './test-render/bar'
-import CandlestickTestRender from './test-render/candlestick'
-import LabelTestRender from './test-render/label'
-import UITestRender from './test-render/ui'
-import IntegrationTestRender from './test-render/integration'
 
 export default class App extends Component {
   render () {
     return (
       <div style={{margin: 20}}>
-        <TestRenderLine />
         {/*}
-        <TestRenderAxes />
-        <TestRenderAxes />
+        <TestRenderLine />
         <TestRenderHistory />
-        */}
-        {/*
-        <IntegrationTestRender />
-        <UITestRender />
-        <LabelTestRender />
-        <CandlestickTestRender />
-        <BarTestRender />
-        <PointTestRender />
-        <LineTestRender />
-        <BackgroundTestRender />
+        <TestRenderAxes />
         */}
       </div>
     )
