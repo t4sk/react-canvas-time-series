92d9850a5025f8dce04432c0609fa4c15175d924
t4sk <tsk.nakamura@gmail.com>
2019-07-31 13:47:59 +0900

    export layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import canvas from "./canvas"
 import Graph from "./graph"
 import History from "./history"
+import layout from "./layout"
 import draggable from "./draggable"
 import zoomable from "./zoomable"
 
 export { default as canvas } from "./canvas"
 export { default as History } from "./history"
 export { default as Graph } from "./graph"
+export { default as layout } from "./layout"
 export { default as draggable } from "./draggable"
 export { default as zoomable } from "./zoomable"
 
@@ -14,6 +16,7 @@ const ReactCanvasTimeSeries = {
   canvas,
   History,
   Graph,
+  layout,
   draggable,
   zoomable,
 }
--- src/layout.js
+++ src/layout.js
@@ -36,3 +36,7 @@ export function getLayout(props) {
     yAxis,
   }
 }
+
+export default {
+  getLayout,
+}
